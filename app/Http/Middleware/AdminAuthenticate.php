40ef74027795bde860011652fd368705bb5e49b5
Al Shahria Tarek <engshakilbsc@gmail.com>
2025-07-08 02:44:17 +0600

    fdhgvdobv

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -10,7 +10,7 @@ class AdminAuthenticate
 {
     /**
      * Handle an incoming request.
-     *
+     *fgnjgnfjgnv
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
